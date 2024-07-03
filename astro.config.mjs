55f7f747e6c3e426d859a72fffa6643beee05447
Dan Levy <justsml@gmail.com>
2024-07-02 20:27:09 -0600

    add tailwind...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,23 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'static',
-	trailingSlash: 'ignore',
-	build: {
-		format: 'directory',
-	},
+  output: 'static',
+  trailingSlash: 'ignore',
+  build: {
+    format: 'directory'
+  },
+  // build: {
+  // 	inlineStylesheets: 'always',
 
-	// build: {
-	// 	inlineStylesheets: 'always',
-
-	// },
-	site: 'https://danlevy.net',
-	integrations: [react(), mdx(), sitemap()],
-	// prefetch: {
-	// 	defaultStrategy: 'viewport', // 'hover',
-	// }
+  // },
+  site: 'https://danlevy.net',
+  integrations: [react(), mdx(), sitemap(), tailwind()]
+  // prefetch: {
+  // 	defaultStrategy: 'viewport', // 'hover',
+  // }
 });
